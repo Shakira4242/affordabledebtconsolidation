2618a06fb826a0191ebc7c91c88b981ea6d4b209
Akash <akashshekara1212@gmail.com>
2023-07-05 16:50:24 -0500

    updated text color

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -299,7 +299,7 @@ export default function Slider() {
                   className='w-full flex justify-center items-center mt-4 mb-40'
                 >
                   <input
-                    className="block w-full rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 shadow-lg border-gray-300 border-2"
+                    className="block w-full rounded-md border-2 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 shadow-lg border-gray-300 border-2"
                     type="phone"
                     autoComplete='tel'
                   ></input>
